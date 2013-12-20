dd4922325e702785b2a10f44670a069d1bfa6bd4
Dinesh Silva <sohandinesh@hotmail.com>
2013-12-20 18:27:24 +0530

    Initial Submission

---
+++ diridharaonline.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>දිරි ධාරා</title>
+    
+    <style>
+		#player-container {}
+	</style>
+</head>
+
+<body>
+	<div id="player-container">
+        
+        <audio src="http://94.23.62.189:5116/stream" controls autoplay></audio>
+        
+    </div>
+</body>
+</html>
